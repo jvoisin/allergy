e913e3622f3fdfad06d2e2923d7313df4ad77370
jvoisin <julien.voisin@dustri.org>
2017-09-18 21:45:09 +0200

    Refactor the index

--- index.php
+++ index.php
@@ -1,39 +1,51 @@
 <?php
+
 require 'vendor/twig/Twig.php';
 require "lib/utils.php";
 
-$loader = new Twig_Loader_Filesystem('./tpl');
-$twig = new Twig_Environment($loader, array());
 
 $dir = get_safe_path($_GET['p'], './data');
 
 if (isset($_POST['move_from']) && isset($_POST['move_to'])) {
     $from = get_safe_path($_POST['move_from'], './data');
     $to = get_safe_path($_POST['move_to'], './data');
-}
-
-if (isset($_POST['del'])) {
+    rename($from, $to);
+} elseif (isset($_POST['del'])) {
     $path = get_safe_path($_GET['p'], './data');
     if ($path != './data') {
         if (is_file($path)) {
             unlink($_POST['del']);
         }
     }
-}
-
-if (isset($_POST['folder_name'])) {
+} elseif (isset($_POST['folder_name'])) {
     $name = preg_replace('[^0-9a-z _()]', '_', $_POST['folder_name']);
     mkdir($dir . DIRECTORY_SEPARATOR . $name);
-}
-
-if (isset($_FILES['images'])) {
+} elseif (isset($_FILES['images'])) {
     foreach (get_uploaded_files_array($_FILES['images']) as $image) {
-        if (move_uploaded_file($image['tmp_name'], $dir . '/' . $image['name'])) {
-        } else {
-            var_dump($image); echo "\n";
+        switch( $image['error'] ) {
+            case UPLOAD_ERR_OK:
+                $path = get_safe_path($dir . '/' . $image['name'], './data');
+                if ($path === $dir . '/' . $image['name']) {
+                    if (move_uploaded_file($image['tmp_name'], $dir . '/' . $image['name'])) {
+                    } else {
+                        var_dump($image);
+                        echo "\n";
+                    }
+                }
+                break;
+            case UPLOAD_ERR_INI_SIZE:
+            case UPLOAD_ERR_FORM_SIZE:
+                die('file too large');
+            case UPLOAD_ERR_PARTIAL:
+                die(' - file upload was not completed.');
+            case UPLOAD_ERR_NO_FILE:
+                die(' - zero-length file uploaded.');
+            default:
+                die(' - internal error #'.$image['error']);
         }
+
     }
 }
 
-echo $twig->render('index.twig', array(
-    'dir_and_files' => get_dir_and_files($dir)));
+$twig = new Twig_Environment(new Twig_Loader_Filesystem('./tpl'), array());
+die($twig->render('index.twig', array('dir_and_files' => get_dir_and_files($dir))));
